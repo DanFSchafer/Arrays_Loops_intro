68c398e2b32f3c91770d1cd14a44bcf86ff738f0
Dan Schafer and Dan Siegel <Guest@epicodus-36.local>
2016-06-13 20:44:21 -0700

    a separate looping exercise, to add code for looping through some input

--- js/styles.js
+++ js/styles.js
@@ -1,17 +1,12 @@
 $(document).ready(function() {
    $("#blanks form").submit(function(event) {
     event.preventDefault();
-    var value1 = $("#animal").val();
-    var value2 = $("#iceCream").val();
-
-
-
-    var array = [];
-    array[0] =value1;
-    array.push(value2);
-    var arraystring = array.toString();
-    $("#output").text(arraystring);
-
+    var icecreams = ["icecream1", "icecream2", "icecream3", "icecream4"];
+    icecreams.forEach(function(icecream) {
+      var list = $('input#' + icecream).val();
+      $("." + icecream).text(list);
+    });
+      $("#story").show(list);
     });
 
 });
